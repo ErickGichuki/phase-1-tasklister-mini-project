486cf2846c568a5f3802a8116cd2cb3905b0a521
+ <+>
2024-03-28 17:08:51 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const taskDescriptionInput = document.getElementById("new-task-description");
+    const taskDescription = taskDescriptionInput.value;
+    
+    createTask(taskDescription);
+    taskDescriptionInput.value = "";
+
+  });
+  function createTask(description){
+    console.log("Creating task:", description);
+  }
 });
